0ec696baa8df69dc9e3886e1fb0bad6f240eb675
Eric-Floberg-Ljungqvist-Nti-johanneberg <119052379+Eric-Floberg-Ljungqvist-Nti-johanneberg@users.noreply.github.com>
2024-04-11 16:32:29 +0200

    Uppdatering
    uppdatering, registrering och login

--- app.rb
+++ app.rb
@@ -1,8 +1,9 @@
 class App < Sinatra::Base
+    enable :sessions
 
     def db
         return @db if @db
-        @db = SQLite3::Database.new('./db/asdas.db')
+        @db = SQLite3::Database.new('./db/movies.db')
         @db.results_as_hash = true
         return @db
     end
@@ -19,11 +20,39 @@ class App < Sinatra::Base
     post '/register' do
         username = params["username"]
         password = params["password"]
+        
+        hashed_password = BCrypt::Password.create(password)
 
         query = 'INSERT INTO login_credentials (username, password) VALUES (?,?)'
-        db.execute(query, username, password)
+        db.execute(query, username, hashed_password)
         redirect "/"
     end
+
+    get '/login' do
+        erb :login
+    end
+
+    post '/login' do
+        
+        username = params["username"]
+        password = params["password"]
+
+        user = db.execute('SELECT * FROM login_credentials WHERE username = ?', username).first
+       
+        if user.nil?
+            return
+        end
+
+        hashed_password = user['password']
+
+        if BCrypt::Password.new(hashed_password) == password
+            session[:user_id] = user['id']
+            session[:username] = user['username']
+            puts session[:username]
+            redirect '/'
+        end
+    end
+
 
     get '/add_movie' do 
         erb :add_movie
@@ -37,8 +66,19 @@ class App < Sinatra::Base
         genre = params["genre"]
         director = params["director"]
 
+        if !(movie_image == nil)
+            File.open('public/img/' + movie_image[:filename], "w") do |f|
+                f.write(movie_image[:tempfile].read)
+            end
+            image_path = "/artwork/" + movie_image[:filename]
+            puts(image_path)
+        else
+            image_path = nil
+            puts("No image path")
+        end
+
         query = 'INSERT INTO movie_db (title, description, year, movie_image, genre, director) VALUES (?,?,?,?,?,?)'
-        db.execute(query, title, description, year, movie_image, genre, director)
+        db.execute(query, title, description, year, image_path, genre, director)
         redirect "/"
     end
 
